11b1e452cd4a0dae1fcabe107164f3db2a00c55a
Dhadhazi <dhadhazi@gmail.com>
2021-02-09 16:16:45 +0100

    Typescript added, and navbar skeleton

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import s from "./NavBar.module.css";
+
+export const NavBar = () => {
+  return <div>About Careers Events Products Support</div>;
+};
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "../components/NavBar/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      Yellow world
+    </div>
+  );
+}
