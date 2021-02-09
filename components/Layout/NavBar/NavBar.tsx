2c099e8153355b590c0cb35f1d499b4564111c74
Dhadhazi <dhadhazi@gmail.com>
2021-02-09 16:45:08 +0100

    Fonts added and navbar working on

--- components/Layout/NavBar/NavBar.tsx
+++ components/Layout/NavBar/NavBar.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import Image from "next/image";
 import s from "./NavBar.module.css";
 
 export const NavBar = () => {
-  return <div>About Careers Events Products Support</div>;
+  return (
+    <div className={s.container}>
+      <div className={s.companyLogo}>loopstudios</div>
+      <div className={s.menubar}>About Careers Events Products Support</div>
+    </div>
+  );
 };
