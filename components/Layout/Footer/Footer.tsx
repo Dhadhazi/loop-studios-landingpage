8ebac84ce5ab9b4d67b36f4b211f3d1a7f1bd8d6
Dhadhazi <dhadhazi@gmail.com>
2021-02-11 12:30:49 +0100

    Footer positioning and bg

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Menu } from "../../shared/Menu/Menu";
 import s from "./Footer.module.scss";
 
 export const Footer = () => {
   return (
     <div className={s.container}>
-      <div>
-        <h2>loopstudios</h2>
-        Menu
+      <div className={s.postitionerContainer}>
+        <div className={s.contentContainer}>
+          <div className={s.logoMenuContainer}>
+            <h2>loopstudios</h2>
+            <Menu />
+          </div>
+          <div className={s.socialContainer}>
+            <div>SOCIAL ICONS</div>
+            <p>© 2021 Loopstudios. All rights reserved.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
