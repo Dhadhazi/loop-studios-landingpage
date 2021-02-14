8fea368cd30bf349c1c0eb0726d3c9c40fe4edab
Dhadhazi <dhadhazi@gmail.com>
2021-02-14 16:03:53 +0100

    Creaton Card splits the string in half

--- components/OurCreations/components/CreationCards.tsx
+++ components/OurCreations/components/CreationCards.tsx
@@ -9,15 +9,42 @@ type Props = {
 export const CreationCards = ({ title, image }: Props) => {
   const [version, setVersion] = React.useState("desktop");
 
+  function splitStringHalfByWord(text: String): String[] {
+    let middle = Math.floor(text.length / 2);
+    let before = text.lastIndexOf(" ", middle);
+    let after = text.indexOf(" ", middle + 1);
+
+    console.log(middle, before, after);
+
+    if (middle - before < after - middle || after === -1) {
+      middle = before;
+    } else {
+      middle = after;
+    }
+
+    const firstHalf = text.substr(0, middle);
+    const secondHalf = text.substr(middle + 1);
+
+    return [firstHalf, secondHalf];
+  }
+
+  console.log(splitStringHalfByWord("The Grid"));
+
   React.useLayoutEffect(() => {
     setVersion(window.innerWidth <= 850 ? "mobile" : "desktop");
   }, []);
+
+  const titleInArray = splitStringHalfByWord(title);
 
   return (
     <div className={s.container}>
       <img src={`${version}/${image}`} className={s.image} />
       <div className={s.titleContainer}>
-        <h3>{title}</h3>
+        <h3>
+          {titleInArray[0]}
+          <br />
+          {titleInArray[1]}
+        </h3>
       </div>
     </div>
   );
