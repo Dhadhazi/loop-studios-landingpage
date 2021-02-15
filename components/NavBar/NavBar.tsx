f07ef336abb7390628c3ad2979f2860e6a920f13
Dhadhazi <dhadhazi@gmail.com>
2021-02-15 17:11:35 +0100

    Mobile menu added

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import { MenuElements } from "../../CONTENT";
 
 export const NavBar = () => {
   const [version, setVersion] = React.useState("desktop");
+  const [overlayMenu, setOverlayMenu] = React.useState("none");
 
   React.useEffect(() => {
     setVersion(window.innerWidth <= 850 ? "mobile" : "desktop");
@@ -27,9 +28,34 @@ export const NavBar = () => {
   } else {
     return (
       <div className={s.mobileContainer}>
+        <div className={s.overlayBackground} style={{ display: overlayMenu }}>
+          <div className={s.overlayLogoAndClose}>
+            <div className={s.companyLogo}>loopstudios</div>
+            <img
+              src="icon-close.svg"
+              width="40px"
+              height="40px"
+              onClick={() => setOverlayMenu("none")}
+            />
+          </div>
+          <div className={s.overlayMenubar}>
+            <ul>
+              {MenuElements.map((menu) => (
+                <li key={menu}>
+                  <a href="#">{menu}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
         <div className={s.companyLogo}>loopstudios</div>
         <div className={s.mobileMenubar}>
-          <img src="icon-hamburger.svg" width="50px" height="35px" />
+          <img
+            src="icon-hamburger.svg"
+            width="50px"
+            height="35px"
+            onClick={() => setOverlayMenu("")}
+          />
         </div>
       </div>
     );
