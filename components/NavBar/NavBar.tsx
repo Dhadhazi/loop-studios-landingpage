21cb108b1f8795e99b27ced373faf90a53d96e00
Dhadhazi <dhadhazi@gmail.com>
2021-02-15 16:50:22 +0100

    Nav bar hamburger icon added

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,18 +3,35 @@ import s from "./NavBar.module.scss";
 import { MenuElements } from "../../CONTENT";
 
 export const NavBar = () => {
-  return (
-    <div className={s.container}>
-      <div className={s.companyLogo}>loopstudios</div>
-      <div className={s.menubar}>
-        <ul>
-          {MenuElements.map((menu) => (
-            <li key={menu}>
-              <a href="#">{menu}</a>
-            </li>
-          ))}
-        </ul>
+  const [version, setVersion] = React.useState("desktop");
+
+  React.useEffect(() => {
+    setVersion(window.innerWidth <= 850 ? "mobile" : "desktop");
+  }, []);
+
+  if (version === "desktop") {
+    return (
+      <div className={s.container}>
+        <div className={s.companyLogo}>loopstudios</div>
+        <div className={s.menubar}>
+          <ul>
+            {MenuElements.map((menu) => (
+              <li key={menu}>
+                <a href="#">{menu}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={s.mobileContainer}>
+        <div className={s.companyLogo}>loopstudios</div>
+        <div className={s.mobileMenubar}>
+          <img src="icon-hamburger.svg" width="50px" height="35px" />
+        </div>
+      </div>
+    );
+  }
 };
