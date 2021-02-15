7514c60a87e9fed5d955477610e9ad42170b5546
Dhadhazi <dhadhazi@gmail.com>
2021-02-15 17:54:03 +0100

    Infocard header to h1 and img has alt

--- components/InfoCard/InfoCard.tsx
+++ components/InfoCard/InfoCard.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import s from "./InfoCard.module.scss";
 
 type Props = {
-  image: String;
-  title: String;
-  description: String;
+  image: string;
+  title: string;
+  description: string;
 };
 
 export const InfoCard = ({ image, title, description }: Props) => {
   return (
     <div className={s.container}>
-      <img src={`desktop/${image}`} className={s.image} />
+      <img
+        src={`desktop/${image}`}
+        className={s.image}
+        alt={`Image for ${title}`}
+      />
       <div className={s.contentContainer}>
-        <header>{title.toUpperCase()}</header>
+        <h1>{title.toUpperCase()}</h1>
         <article>{description}</article>
       </div>
     </div>
